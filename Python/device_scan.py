acd25f281985b01c8ef4506446c6a1fa02b7f239
mvartani76 <mike.vartanian@gmail.com>
2018-10-03 02:52:07 +0000

    Added code to print number of unique MAC addresses in front of the corresponding MAC address.

--- Python/device_scan.py
+++ Python/device_scan.py
@@ -9,9 +9,11 @@ devices = set()
 def PacketHandler(pkt):
         if pkt.haslayer(Dot11):
                 dot11_layer = pkt.getlayer(Dot11)
+		# only add devices not already in the set
                 if dot11_layer.addr2 and (dot11_layer.addr2 not in devices):
                         devices.add(dot11_layer.addr2)
-                        print dot11_layer.addr2
+			# print the number of unique devices followed by the MAC address
+                        print len(devices), dot11_layer.addr2
 
 sniff(iface = sys.argv[1], count = int(sys.argv[2]), prn = PacketHandler)
 
