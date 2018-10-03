517d40d0e24a04991a7bcfeefc0e2f70a33d41b9
mvartani76 <mike.vartanian@gmail.com>
2018-10-03 15:56:44 +0000

    Updated file to be called from external pubsub code. Commented out the command line arguments. Added a class to be able to access items individually in pubsub code.

--- Python/device_scan.py
+++ Python/device_scan.py
@@ -12,6 +12,15 @@ from load_blacklist import load_blacklist
 
 devices = set()
 
+# Create a global class to store scanned/collected variables to push to pubsub code
+class wifiScan:
+	unique_count = 0
+	log_time = str(int(time.time()))
+	mac_addr = ""
+	payload_name = ""
+	rssi = -256
+	vendor = "UNKNOWN"
+
 def build_packetHandler(time_format, blacklist):
 	def packetHandler(pkt):
         	if pkt.haslayer(Dot11):
@@ -39,11 +48,18 @@ def build_packetHandler(time_format, blacklist):
                         	devices.add(dot11_layer.addr2)
 				# print the number of unique devices followed by the MAC address
  				print len(devices), log_time, dot11_layer.addr2, dot11_layer.payload.name, rssi_val, vendor
+
+				# load values into class object so they can be put into json object in pubsub code
+				wifiScan.unique_count = len(devices)
+				wifiScan.log_time = log_time
+				wifiScan.mac_addr = dot11_layer.addr2
+				wifiScan.payload_name = dot11_layer.payload.name
+				wifiScan.rssi = rssi_val
+				wifiScan.vendor = vendor
 	return packetHandler
 
-
+# Uncomment the following lines if you want to test this code independently
 # load in the blacklist of MAC addresses
-blacklist = load_blacklist(sys.argv[4])
-built_packetHandler = build_packetHandler(sys.argv[3], blacklist)
-sniff(iface = sys.argv[1], count = int(sys.argv[2]), prn = built_packetHandler)
-
+#blacklist = load_blacklist(sys.argv[4])
+#built_packetHandler = build_packetHandler(sys.argv[3], blacklist)
+#sniff(iface = sys.argv[1], count = int(sys.argv[2]), prn = built_packetHandler)
