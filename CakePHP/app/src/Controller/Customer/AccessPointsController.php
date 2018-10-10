5e45ebf1d26e98e7af67daba43c1076ce37d9d6f
Mike Vartanian <mike.vartanian@gmail.com>
2018-10-09 23:17:05 -0400

    Using bin/cake bake version of controller file.

--- CakePHP/app/src/Controller/Customer/AccessPointsController.php
+++ CakePHP/app/src/Controller/Customer/AccessPointsController.php
@@ -2,69 +2,111 @@
 namespace App\Controller\Customer;
 
 use App\Controller\AppController;
-use App\Model\Entity\User;
-use Cake\Core\Configure;
-use Cake\ORM\TableRegistry;
-use Cake\Event\Event;
-use Cake\Network\Exception\ForbiddenException;
 
 /**
- * Access Points Controller
+ * AccessPoints Controller
  *
- * @property \App\Model\Table\AccessPointsTable $Access Points
+ * @property \App\Model\Table\AccessPointsTable $AccessPoints
+ *
+ * @method \App\Model\Entity\AccessPoint[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
 class AccessPointsController extends AppController
 {
-    /**
-     * Controller Name
-     *
-     * @var string
-     */
-    public $name = 'AccessPoints';
 
     /**
      * Index method
      *
-     * @return void
+     * @return \Cake\Http\Response|void
      */
     public function index()
     {
+        $this->paginate = [
+            'contain' => ['Customers']
+        ];
+        $accessPoints = $this->paginate($this->AccessPoints);
 
+        $this->set(compact('accessPoints'));
     }
 
     /**
      * View method
      *
      * @param string|null $id Access Point id.
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function view($id = null)
     {
+        $accessPoint = $this->AccessPoints->get($id, [
+            'contain' => ['Customers']
+        ]);
+
+        $this->set('accessPoint', $accessPoint);
     }
 
     /**
      * Add method
      *
-     * @return void Redirects on successful add, renders view otherwise.
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
      */
     public function add()
     {
+        $accessPoint = $this->AccessPoints->newEntity();
+        if ($this->request->is('post')) {
+            $accessPoint = $this->AccessPoints->patchEntity($accessPoint, $this->request->getData());
+            if ($this->AccessPoints->save($accessPoint)) {
+                $this->Flash->success(__('The access point has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The access point could not be saved. Please, try again.'));
+        }
+        $customers = $this->AccessPoints->Customers->find('list', ['limit' => 200]);
+        $this->set(compact('accessPoint', 'customers'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Access Point id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $accessPoint = $this->AccessPoints->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $accessPoint = $this->AccessPoints->patchEntity($accessPoint, $this->request->getData());
+            if ($this->AccessPoints->save($accessPoint)) {
+                $this->Flash->success(__('The access point has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The access point could not be saved. Please, try again.'));
+        }
+        $customers = $this->AccessPoints->Customers->find('list', ['limit' => 200]);
+        $this->set(compact('accessPoint', 'customers'));
     }
 
     /**
      * Delete method
      *
      * @param string|null $id Access Point id.
-     * @return \Cake\Network\Response|null Redirects to index.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete($id = null)
     {
-    }
+        $this->request->allowMethod(['post', 'delete']);
+        $accessPoint = $this->AccessPoints->get($id);
+        if ($this->AccessPoints->delete($accessPoint)) {
+            $this->Flash->success(__('The access point has been deleted.'));
+        } else {
+            $this->Flash->error(__('The access point could not be deleted. Please, try again.'));
+        }
 
-    public function company()
-    {
-
+        return $this->redirect(['action' => 'index']);
     }
 }
