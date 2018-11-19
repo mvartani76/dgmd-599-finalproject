274dada9cfd948387bddbb85e624edf9dca2d824
Mike Vartanian <mike.vartanian@gmail.com>
2018-11-18 21:11:34 -0500

    Initial add of image lightbox js code.

---
+++ CakePHP/app/webroot/js/image-lightbox.js
@@ -0,0 +1,78 @@
+$(function() {
+    $(document.body).off('click', '.lightbox-image').on('click', '.lightbox-image', function(e) {
+
+
+
+        if ($(this).data('title')) {
+            var title = $(this).data('title');
+        } else {
+            var title = '';
+        }
+
+        if ($(this).data('description')) {
+            var description = $(this).data('description');
+        } else {
+            var description = '';
+        }
+
+
+        var misc = '';
+        var imageUrl = $(this).attr('href');
+
+        if (!$(this).data('restrict-edit')) {
+            if ($(this).data('media-id')) {
+                var editLink = '<a href="/marketing/content/editMediaLibrary?ids[0]=' + $(this).data('media-id') + '" class="btn btn-default"><i class="fa fa-edit"></i>&nbsp; Edit Image</a>';
+            } else {
+                var editLink = '';
+            }
+        } else {
+            var editLink = '';
+        }
+
+        if (title || description) {
+
+            var content = '';
+
+            if (title) {
+                content = '<p><strong>' + title + '</strong></p>';
+            }
+
+            if (description) {
+                content += '<p>' + description + '</p>';
+            }
+
+
+            var misc = '<div style="background: #f7f7f7 none repeat scroll 0 0;display: block; margin: 6px 6px 0; padding: 5px;">' +
+                content +
+                '</div>';
+        }
+
+        var content = '<div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+            <div class="modal-dialog" style="/**width: 652px !important;**/">\
+                <div class="modal-content">\
+                    <div class="modal-header">\
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
+                        <h4 class="modal-title" id="myModalLabel">Viewing Larger Image</h4>\
+                    </div>\
+                    <div class="modal-body" style="text-align: center; padding: 5px !important; ">\
+                        <img style="max-height: 700px; max-width: 100%;" src="'+imageUrl+'" id="imagepreview"  >\
+                        ' + misc + '\
+                    </div>\
+                    <div class="modal-footer">\
+                        ' + editLink + '\
+                        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times-circle"></i> &nbsp;Close Window</button>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>';
+
+
+        $('#imagemodal').remove();
+        $('body').append(content);
+
+        $('#imagemodal').modal('show');
+
+        e.preventDefault();
+        return false;
+    });
+});
