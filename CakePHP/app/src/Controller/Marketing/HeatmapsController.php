d97bfc9ba8552001311bf541117d1564558a04ae
mvartani76 <mike.vartanian@gmail.com>
2018-12-11 13:45:24 -0500

    Test build where I generate the random numbers used for the heatmap in the controller instead of in the view file javascript. Also fetching data from aws dynamo db but not using yet.

--- CakePHP/app/src/Controller/Marketing/HeatmapsController.php
+++ CakePHP/app/src/Controller/Marketing/HeatmapsController.php
@@ -2,6 +2,9 @@
 namespace App\Controller\Marketing;
 
 use App\Controller\AppController;
+use Aws\Sdk as AwsSdk;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
 
 /**
  * Heatmaps Controller
@@ -142,6 +145,155 @@ class HeatmapsController extends AppController
             'contain' => ['access_points', 'floorplans_library']
         ]);
 
+        // Pass in the AWS credentials from the .env file
+        $sdk = new AwsSdk([
+            'region'   => env('AWS_DYNAMODB_REGION', 'us-west-2'),
+            'version'  => env('AWS_DYNAMODB_VERSION', 'latest'),
+            'credentials' => [
+                'key' => env('AWS_DYNAMODB_CREDENTIALS_KEY', null),
+                'secret'  => env('AWS_DYNAMODB_CREDENTIALS_SECRET', null),
+            ],
+        ]);
+
+        if (isset($this->request->query['ajax']) && $this->request->query['ajax'] == true) {
+            if ($this->request->is('ajax')) {
+                $this->viewBuilder()->layout('ajax_paging');
+            }
+        }
+        if (isset($this->request->query['mdl'])) {
+            if ($this->request->is('ajax')) {
+                $this->viewBuilder()->layout('ajax_paging');
+                $this->set('contentKey', $this->request->query['mdl']);
+            }
+        }
+
+        if (!empty($this->request->data['action']) && $this->request->data['action'] == 'updateStats') {
+
+            $startDate  = urldecode($this->request->data['starttime']);
+            $endDate    = urldecode($this->request->data['endtime']);
+
+            // Keeping in unix time (in msec) as that is what is stored in dynamoDB
+            $periodStart = strtotime($startDate)*1000;
+            $periodEnd   = strtotime($endDate)*1000;
+
+        } else {
+
+            $periodStart  = strtotime('-7 days')*1000;
+            $periodEnd    = time()*1000;
+        }
+
+        $periodStartRaw = date('Y-m-d h:i:s', $periodStart/1000);
+        $periodEndRaw   = date('Y-m-d h:i:s', $periodEnd/1000);
+
+
+        // Create a DynamoDb instance
+        $dynamodb = $sdk->createDynamoDb();
+        $marshaler = new Marshaler();
+
+        $tableName = 'wdds_testdata';
+
+        // Need to format the access point mac address with colons as this is
+        // how it is stored in dynamo dB
+        $apstr = join(':', str_split($heatmap->access_point->mac_addr,2));
+
+        // Set the filter expression for mac address to be the selected access point mac address
+        // Creating the JSON string that marshjson would have done
+        $eav = array(":mmaacc"=>array("S"=>$apstr));
+
+        // Set the ExpressionAttributeValues for the time based query.
+        // Note that the numbers for starttime/endtime need to be surrounded by double quotation marks.
+        $eav_time = array(":mmaacc"=>array("S"=>$apstr),":starttime"=>array("N"=>"$periodStart"),":endtime"=>array("N"=>"$periodEnd"));
+
+
+        // Set the params for the dB query to show scanresults over the selected time period
+        // Used for displaying unique devices over a given time
+        $unique_params_time = [
+            'TableName' => $tableName,
+            'KeyConditionExpression' => 'ap_mac_addr = :mmaacc AND log_time BETWEEN :starttime AND :endtime',
+            'ExpressionAttributeValues' => $eav_time,
+            'Select' => "ALL_ATTRIBUTES"
+            ];
+
+        // Set the params for the dB scan to show results for access point
+        // Conditionally set the params using ExclusiveStartKey if available
+        // Limit the results to 15
+        if (isset($key)) {
+            $params = [
+                'TableName' => $tableName,
+                'ProjectionExpression' => 'ap_mac_addr, payload.mac_addr, payload.vendor, payload.rssi, log_time',
+                'KeyConditionExpression' => 'ap_mac_addr = :mmaacc',
+                'ExpressionAttributeValues' => $eav,
+                'Limit' => 15,
+                'ExclusiveStartKey' => $key
+            ];
+        } else {
+            $params = [
+                'TableName' => $tableName,
+                'ProjectionExpression' => 'payload.rssi',
+                'KeyConditionExpression' => 'ap_mac_addr = :mmaacc',
+                'ExpressionAttributeValues' => $eav,
+                'Limit' => 15
+            ];
+        }
+
+        $scanResults = [];
+
+
+        // Query the scan results for the given access point but limit to 15 items
+        try {
+            $result = $dynamodb->query($params);
+            foreach ($result['Items'] as $mac_addr) {
+                $scanResults[] = $marshaler->unmarshalItem($mac_addr);
+            }
+
+        } catch (DynamoDbException $e) {
+            echo "Unable to query:\n";
+            echo $e->getMessage() . "\n";
+            die();
+        }
+
+        //$scanResults = (object) $scanResults;
+
+        //pr($scanResults);
+
+        // Set the previous last evaluated key to be able to navigate back to the previous page
+        /*if (isset($lastevalkey)) {
+            $prevlastvalkey = $lastevalkey;
+        } else {
+            $prevlastvalkey = $result['LastEvaluatedKey'];
+        }
+        $lastevalkey = $result['LastEvaluatedKey'];
+
+        $this->set('accessPoint', $accessPoint);
+
+        $this->set(compact('scanResults','lastevalkey', 'prevlastvalkey', 'page'));
+        $this->set(compact('totalScanCount', 'totalScanCount_time', 'totalUniqueDevices', 'totalUniqueDevices_time'));
+        $this->set(compact('notes', 'periodStartRaw', 'periodEndRaw'));
+
+        // Need to serialize the variables to have them visible for the JSON response
+        $this->set('_serialize', ['totalScanCount', 'totalScanCount_time', 'totalUniqueDevices', 'totalUniqueDevices_time']);
+*/
+        $points = [];
+        $maxval = 0;
+        for ($i=0; $i<200; $i++){
+            $val = floor(mt_rand(0,100));
+            $maxval = max($maxval, $val);
+          //$point = {
+          //  x: Math.floor(Math.random()*width),
+          //  y: Math.floor(Math.random()*height),
+          //  value: val
+          //};
+            //pr($val);
+            $point['x'] = floor(mt_rand(0,800));
+            $point['y'] = floor(mt_rand(0,400));
+            $point['value'] = $val;
+            $mypoints['data'][] = $point;
+        }
+        $mypoints['max'] = $maxval;
+//pr($points);
+
         $this->set('heatmap', $heatmap);
+        $this->set('mypoints', json_encode($mypoints));
+
     }
 }
