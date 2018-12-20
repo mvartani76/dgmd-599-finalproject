66057862cfb0251098d486abd9ebfaf9b03fb909
mvartani76 <mike.vartanian@gmail.com>
2018-12-20 05:54:50 +0000

    Added helper functions to strip semicolons and truncate.

--- Python/hash_addr.py
+++ Python/hash_addr.py
@@ -4,4 +4,9 @@ import hashlib
 # an encrypted string
 def hash_addr(mac_addr):
 	return hashlib.sha256(mac_addr.encode()).hexdigest()
-  
+
+def strip_semicolons(mac_addr):
+	return mac_addr.replace(':','')
+
+def trunc_hash(mac_addr, len):
+	return hash_addr(strip_semicolons(mac_addr))[:len]
