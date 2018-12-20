246e32df7ae1fcc2748d53ad06c14108d9b31007
mvartani76 <mike.vartanian@gmail.com>
2018-12-20 06:21:20 +0000

    Added function to insert semi-colons into string as that is what dynamodb is expecting. Also added comments for other methods.

--- Python/hash_addr.py
+++ Python/hash_addr.py
@@ -5,8 +5,14 @@ import hashlib
 def hash_addr(mac_addr):
 	return hashlib.sha256(mac_addr.encode()).hexdigest()
 
+# strip semi-colons from mac address string
 def strip_semicolons(mac_addr):
 	return mac_addr.replace(':','')
 
+# hash the mac address and truncate to len characters
 def trunc_hash(mac_addr, len):
 	return hash_addr(strip_semicolons(mac_addr))[:len]
+
+# Convert string to mac address format with semicolons every two characters
+def conv_mac(str, sep=':'):
+    return sep.join([str[i:i+2] for i in range(0, len(str), 2)])
